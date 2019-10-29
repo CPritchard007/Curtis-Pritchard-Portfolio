d07b4e02fc4f9532b5ff064cf2a5a2d035a34b5d
Curtis Pritchard <curtis.pritchard@stclairconnect.ca>
2019-10-29 18:57:40 -0400

    webpage now expands individual elements if its width does not meet the requirement.

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Message {
+  header: string;
+  message: string;
+  image?:string;
+}
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
-
-  constructor() { }
+ messages: Message[]
+  constructor() {
+   this.messages = [{
+     header: "Curtis Pritchard",
+     message: "message"
+   }];
+  }
 
   ngOnInit() {
   }
