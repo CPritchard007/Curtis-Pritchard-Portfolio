c1cc93a2eb1b68c63f249b7b64c923a9c92693b5
cpritchard007 <cpritchard649@gmail.com>
2019-11-13 22:22:09 -0500

    refactored all css files

--- src/app/languages/languages.component.ts
+++ src/app/languages/languages.component.ts
@@ -34,111 +34,15 @@ export class LanguagesComponent implements OnInit {
     }, {
       name: 'CSharp',
       icon: '../assets/language-icons/csharp-original.svg'
-    },{
-      name: 'JavaScript',
-      icon: '../assets/language-icons/javascript-original.svg'
     }, {
-      name: 'Java',
-      icon: '../assets/language-icons/java-original.svg'
+      name: 'Python',
+      icon: '../assets/language-icons/python-original.svg'
     }, {
-      name: 'php',
-      icon: '../assets/language-icons/php-original.svg'
+      name: 'Laravel',
+      icon: '../assets/language-icons/laravel-original.svg'
     }, {
-      name: 'HTML5',
-      icon: '../assets/language-icons/html5-original.svg'
-    }, {
-      name: 'Android',
-      icon: '../assets/language-icons/android-original.svg'
-    }, {
-      name: 'Angular',
-      icon: '../assets/language-icons/angular-original.svg'
-    }, {
-      name: 'CSharp',
-      icon: '../assets/language-icons/csharp-original.svg'
-    },{
-      name: 'JavaScript',
-      icon: '../assets/language-icons/javascript-original.svg'
-    }, {
-      name: 'Java',
-      icon: '../assets/language-icons/java-original.svg'
-    }, {
-      name: 'php',
-      icon: '../assets/language-icons/php-original.svg'
-    }, {
-      name: 'HTML5',
-      icon: '../assets/language-icons/html5-original.svg'
-    }, {
-      name: 'Android',
-      icon: '../assets/language-icons/android-original.svg'
-    }, {
-      name: 'Angular',
-      icon: '../assets/language-icons/angular-original.svg'
-    }, {
-      name: 'CSharp',
-      icon: '../assets/language-icons/csharp-original.svg'
-    },{
-      name: 'JavaScript',
-      icon: '../assets/language-icons/javascript-original.svg'
-    }, {
-      name: 'Java',
-      icon: '../assets/language-icons/java-original.svg'
-    }, {
-      name: 'php',
-      icon: '../assets/language-icons/php-original.svg'
-    }, {
-      name: 'HTML5',
-      icon: '../assets/language-icons/html5-original.svg'
-    }, {
-      name: 'Android',
-      icon: '../assets/language-icons/android-original.svg'
-    }, {
-      name: 'Angular',
-      icon: '../assets/language-icons/angular-original.svg'
-    }, {
-      name: 'CSharp',
-      icon: '../assets/language-icons/csharp-original.svg'
-    },{
-      name: 'JavaScript',
-      icon: '../assets/language-icons/javascript-original.svg'
-    }, {
-      name: 'Java',
-      icon: '../assets/language-icons/java-original.svg'
-    }, {
-      name: 'php',
-      icon: '../assets/language-icons/php-original.svg'
-    }, {
-      name: 'HTML5',
-      icon: '../assets/language-icons/html5-original.svg'
-    }, {
-      name: 'Android',
-      icon: '../assets/language-icons/android-original.svg'
-    }, {
-      name: 'Angular',
-      icon: '../assets/language-icons/angular-original.svg'
-    }, {
-      name: 'CSharp',
-      icon: '../assets/language-icons/csharp-original.svg'
-    },{
-      name: 'JavaScript',
-      icon: '../assets/language-icons/javascript-original.svg'
-    }, {
-      name: 'Java',
-      icon: '../assets/language-icons/java-original.svg'
-    }, {
-      name: 'php',
-      icon: '../assets/language-icons/php-original.svg'
-    }, {
-      name: 'HTML5',
-      icon: '../assets/language-icons/html5-original.svg'
-    }, {
-      name: 'Android',
-      icon: '../assets/language-icons/android-original.svg'
-    }, {
-      name: 'Angular',
-      icon: '../assets/language-icons/angular-original.svg'
-    }, {
-      name: 'CSharp',
-      icon: '../assets/language-icons/csharp-original.svg'
+      name: 'Swift',
+      icon: '../assets/language-icons/swift-original.svg'
     }];
   }
 
