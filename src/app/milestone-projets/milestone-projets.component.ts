f036e12366a0a1dd6dbc08c7e2e47540cf1202d5
cpritchard007 <cpritchard649@gmail.com>
2019-11-26 09:56:34 -0500

    added updated milestone animation added new milestone item grow milestone item in mobile view

--- src/app/milestone-projets/milestone-projets.component.ts
+++ src/app/milestone-projets/milestone-projets.component.ts
@@ -16,9 +16,10 @@ export class MilestoneProjetsComponent implements OnInit {
 
   ngOnInit() {
     this.milestoneProjects = [
-      {name: 'TypeKnights', description: 'TypeKnight was I game I worked on in Java, with my partner David froelich. This game utilizes the use of the keyboard in/out functionality into a fun little typing rpg.', image: 'https://www.researchgate.net/profile/Laurianne_Sitbon/publication/41201766/figure/fig5/AS:669565243641859@1536648202119/Process-of-random-projection-to-compute-term-and-documents-matrices.png', language: 'java'},
+      {name: 'TypeKnights', description: 'TypeKnight was I game I worked on in Java, with my partner David froelich. This game utilizes the use of the keyboard in/out functionality into a fun little typing rpg. xxxxxxxxx xxxxxxxxxxx xxxxxxxxxxxxx xxxxxxxxx xxxxxxxxxxx xxxxxxxxxx xxxxxxxxxx xxxxxxxxxx xxxxx xxxxxxx xxxxxx xxxxxxxx xxxxxx', image: 'https://www.researchgate.net/profile/Laurianne_Sitbon/publication/41201766/figure/fig5/AS:669565243641859@1536648202119/Process-of-random-projection-to-compute-term-and-documents-matrices.png', language: 'java'},
       {name: 'Portfolio', description: 'This is the Portfolio Description', image: 'https://www.researchgate.net/profile/Laurianne_Sitbon/publication/41201766/figure/fig5/AS:669565243641859@1536648202119/Process-of-random-projection-to-compute-term-and-documents-matrices.png', language: 'java'},
-      {name: 'Android', description: 'This is the Portfolio Description', image: 'https://www.researchgate.net/profile/Laurianne_Sitbon/publication/41201766/figure/fig5/AS:669565243641859@1536648202119/Process-of-random-projection-to-compute-term-and-documents-matrices.png', language: 'java'}
+      {name: 'Android', description: 'This is the Portfolio Description', image: 'https://www.researchgate.net/profile/Laurianne_Sitbon/publication/41201766/figure/fig5/AS:669565243641859@1536648202119/Process-of-random-projection-to-compute-term-and-documents-matrices.png', language: 'java'},
+      {name: 'First Robotics assistant-programmer', description: 'I currently assist the Tecumseh vista robotics programming team. I lend them my knowlege of Java, Python and RaspbianLite so that they may be able to complete a functioning robot for the coming competitions.', image: 'https://www.researchgate.net/profile/Laurianne_Sitbon/publication/41201766/figure/fig5/AS:669565243641859@1536648202119/Process-of-random-projection-to-compute-term-and-documents-matrices.png', language: 'java'}
     ];
   }
 
